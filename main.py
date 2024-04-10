215002891dacba2efbeed2ce0f27104d3baeccee
qwarkx <yu7rwa@gmail.com>
2024-04-10 14:19:03 +0200

    updated exporter - working

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+
+
+import os
 import argparse
+
+from win32com import client
+from pathlib import Path
 
 from datetime import datetime
 import time
@@ -18,13 +24,59 @@ def main():
     folder_path = "CTTpymat_" + "SIG_Tester" + "_V800"
 
     # NOTE: Select waht to read, device[1] or sensor[0]
-    parser.add_argument('--folder_path', type=int, default=folder_path)
+    parser.add_argument('--folder_path', type=str, default=folder_path)
     parser.add_argument('--rtc', type=str, default=rtc)
     args = parser.parse_args()
 
 
     # Main code which ia run
     run_program(args)
+
+
+
+def convert_docx_to_pdf(docx_path, pdf_path):
+    word = client.Dispatch('Word.Application')
+    doc = word.Documents.Open(docx_path)
+    doc.SaveAs(pdf_path, FileFormat=17)
+    doc.Close()
+    word.Quit()
+
+# Example usage
+# convert_docx_to_pdf('example.docx', 'example.pdf')
+
+def batch_convert_docx_to_pdf(file_list, output_folder = None):
+
+    if not file_list:
+        print("No files to convert.")
+        return
+
+    output_folder = os.path.join(os.path.dirname(file_list[0]), "PDF")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    try:
+        print("Converting doc to PDF files")
+
+        for docx_path in file_list:
+            pdf_path = os.path.join(output_folder, f'{os.path.splitext(os.path.basename(docx_path))[0]}.pdf')
+            print('Converting file: * ' + pdf_path)
+            convert_docx_to_pdf(docx_path, pdf_path)
+    except Exception as e:
+        print("Something went wrong in printing, see:  ", e)
+    print('Finished converting the files')
+
+def list_files(folder_path, extensions=['.docx']):
+    files = []
+    try:
+        for file_name in os.listdir(folder_path):
+            if any(file_name.endswith(ext) for ext in extensions):
+                file_path = os.path.join(folder_path, file_name)
+                print(file_path)
+                files.append(file_path)
+    except Exception as e:
+        print('Files could not be located, see:', e)
+    return files
+
 
 def run_program(args):
 
@@ -37,19 +89,19 @@ def run_program(args):
 
 
     try:
-        print("generating PDF from word files")
+        print("Get file paths")
+        file_paths = 'D:\[Part 100 ] - Device documents V65\[Part 016 ] - Technical Files\SIQ PDF files\\testing'
 
+        files = list_files(file_paths)
+        batch_convert_docx_to_pdf(files)
+
+        print("generating PDF from word files", files)
     except Exception as e:
         print('Something happened while exporting, see:  ', e)
-
-
     try:
         print("Exporting the file list of Technical Files sending")
-
     except Exception as e:
         print('Something happened while making the TF file list, see:  ', e)
-
-
 
 
 
